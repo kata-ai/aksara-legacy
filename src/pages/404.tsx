6243ede946071e8b0c948ba584c353fdef485c8f
Resi Respati <resir014@gmail.com>
2018-11-09 16:10:29 +0700

    refactor: move index page to pages folder

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,6 +8,7 @@ import NotFoundWrapper from 'components/NotFoundWrapper';
 import styled from 'utils/styled';
 import { SiteMetadata } from 'interfaces/gatsby';
 import { RouteComponentProps } from '@reach/router';
+import PageLayout from 'layouts/pageLayout';
 
 interface Props extends RouteComponentProps {
   data: {
@@ -18,20 +19,22 @@ interface Props extends RouteComponentProps {
 }
 
 const NotFoundPage: React.SFC<Props> = ({ data }) => (
-  <Page>
-    <Helmet>
-      <title>404: Page not found. &middot; {data.site.siteMetadata.title}</title>
-    </Helmet>
-    <NotFoundWrapper>
-      <Inner>
-        <Title>404</Title>
-        <Body>We can't find the page you're looking for.</Body>
-        <Body>
-          <Link to="/">Go back?</Link>
-        </Body>
-      </Inner>
-    </NotFoundWrapper>
-  </Page>
+  <PageLayout>
+    <Page>
+      <Helmet>
+        <title>404: Page not found. &middot; {data.site.siteMetadata.title}</title>
+      </Helmet>
+      <NotFoundWrapper>
+        <Inner>
+          <Title>404</Title>
+          <Body>We can't find the page you're looking for.</Body>
+          <Body>
+            <Link to="/">Go back?</Link>
+          </Body>
+        </Inner>
+      </NotFoundWrapper>
+    </Page>
+  </PageLayout>
 );
 
 export default NotFoundPage;
@@ -61,12 +64,10 @@ const Inner = styled('div')`
 const Title = styled('h1')`
   font-size: 5rem;
   margin: 0;
-  color: ${props => props.theme.colors.gray.calm};
 `;
 
 const Body = styled('p')`
   margin-top: 0.5rem;
   margin-bottom: 0;
   font-size: 1.25rem;
-  color: ${props => props.theme.colors.gray.calm};
 `;
--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -1,68 +1,9 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
-import { RouteComponentProps } from '@reach/router';
-
-import Page from 'components/Page';
-import Container from 'components/Container';
-import DocsWrapper from 'components/DocsWrapper';
-import FooterWrapper from 'components/FooterWrapper';
-import Footer from 'components/Footer';
 
 import HomeLayout from '../layouts/homeLayout';
-import Banner from '../components/home/Banner';
 
-interface PageTemplateProps extends RouteComponentProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-        description: string;
-        author: {
-          name: string;
-          url: string;
-        };
-      };
-    };
-  };
-}
-
-const PageTemplate: React.SFC<PageTemplateProps> = () => {
-  return (
-    <HomeLayout>
-      <Page docsPage>
-        <Helmet>
-          <meta property="og:title" content="Home" />
-        </Helmet>
-        <DocsWrapper>
-          <Banner />
-        </DocsWrapper>
-        <FooterWrapper>
-          <Container>
-            <Footer />
-          </Container>
-        </FooterWrapper>
-      </Page>
-    </HomeLayout>
-  );
+const HomeTemplate: React.SFC = ({ children }) => {
+  return <HomeLayout>{children}</HomeLayout>;
 };
 
-export default PageTemplate;
-
-export const query = graphql`
-  query HomeTemplateQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        siteUrl
-        keywords
-        author {
-          name
-          url
-          email
-        }
-      }
-    }
-  }
-`;
+export default HomeTemplate;
