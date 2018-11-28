d7b8dc40cd5778e3c0805f2f0bc2376e11520970
Resi Respati <resir014@gmail.com>
2018-11-28 18:49:04 +0700

    docs: complete(ish) type scale

--- src/components/docs/foundation/TypographyTable.tsx
+++ src/components/docs/foundation/TypographyTable.tsx
@@ -94,10 +94,7 @@ const TypographyTable: React.FC = () => (
             </Pre>
           </td>
           <td>
-            <P>
-              Headings that identify key functionality. Use to explain activity state (eg: drawer,
-              popup)
-            </P>
+            <P>Low Level Heading. Use for Card title.</P>
           </td>
         </tr>
         <tr>
@@ -117,8 +114,8 @@ const TypographyTable: React.FC = () => (
           </td>
           <td>
             <P>
-              Headings that identify key functionality. Use to explain activity state (eg: drawer,
-              popup)
+              - Empty state title
+              <br />- Field Group heading.
             </P>
           </td>
         </tr>
@@ -141,6 +138,82 @@ const TypographyTable: React.FC = () => (
           </td>
           <td>
             <P>Table Heading</P>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <TextXLarge>Body - xLarge</TextXLarge>
+          </td>
+          <td>
+            <Pre style={{ margin: 0 }}>
+              <strong>font-size:</strong> 24px / 1.5rem;
+              <br />
+              <strong>font-weight:</strong> 300;
+              <br />
+              <strong>letter-spacing:</strong> 0em;
+              <br />
+              <strong>color:</strong> neutral08 / #484C4F;
+            </Pre>
+          </td>
+          <td>
+            <P>Short Description beneath title</P>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <TextLarge>Body - Large</TextLarge>
+          </td>
+          <td>
+            <Pre style={{ margin: 0 }}>
+              <strong>font-size:</strong> 24px / 1.5rem;
+              <br />
+              <strong>font-weight:</strong> 300;
+              <br />
+              <strong>letter-spacing:</strong> 0em;
+              <br />
+              <strong>color:</strong> neutral08 / #484C4F;
+            </Pre>
+          </td>
+          <td>
+            <P>Anything from a couple of words to long description fields.</P>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <TextMedium>Body - Medium</TextMedium>
+          </td>
+          <td>
+            <Pre style={{ margin: 0 }}>
+              <strong>font-size:</strong> 14px / 0.875rem;
+              <br />
+              <strong>font-weight:</strong> 300;
+              <br />
+              <strong>letter-spacing:</strong> 0em;
+              <br />
+              <strong>color:</strong> neutral08 / #484C4F;
+            </Pre>
+          </td>
+          <td>
+            <P>Anything from a couple of words to long description fields.</P>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <TextSmall>Body - Small</TextSmall>
+          </td>
+          <td>
+            <Pre style={{ margin: 0 }}>
+              <strong>font-size:</strong> 12px / 0.75rem;
+              <br />
+              <strong>font-weight:</strong> 300;
+              <br />
+              <strong>letter-spacing:</strong> 0.25em;
+              <br />
+              <strong>color:</strong> neutral08 / #484C4F;
+            </Pre>
+          </td>
+          <td>
+            <P>Used for help text and secondary content.</P>
           </td>
         </tr>
       </tbody>
@@ -170,8 +243,8 @@ const Heading5 = styled(H5)`
 
 const Label = styled('span')`
   font-size: ${fontSizes.hecto};
-  font-weight: 700;
   line-height: ${lineHeights.hecto};
+  font-weight: 700;
   color: ${colors.neutral06};
   letter-spacing: -0.01em;
   text-transform: uppercase;
@@ -179,6 +252,35 @@ const Label = styled('span')`
 
 const Pre = styled('pre')`
   white-space: pre !important;
+`;
+
+const TextXLarge = styled('span')`
+  font-size: ${fontSizes.tera};
+  line-height: ${lineHeights.tera};
+  font-weight: 300;
+  color: ${colors.neutral08};
+`;
+
+const TextLarge = styled('span')`
+  font-size: ${fontSizes.giga};
+  line-height: ${lineHeights.giga};
+  font-weight: 300;
+  color: ${colors.neutral08};
+`;
+
+const TextMedium = styled('span')`
+  font-size: ${fontSizes.hecto};
+  line-height: ${lineHeights.hecto};
+  font-weight: 300;
+  color: ${colors.neutral08};
+`;
+
+const TextSmall = styled('span')`
+  font-size: ${fontSizes.deka};
+  line-height: ${lineHeights.deka};
+  font-weight: 300;
+  letter-spacing: 0.25em;
+  color: ${colors.neutral08};
 `;
 
 export default TypographyTable;
